3c7b1e14cf944406bdf8cf6c7f664ee45f349d43
Alexei Kochetov <alex@kochetov.com>
2014-05-23 16:41:49 +0400

    Beautify

--- EK-addressbook-tests/src/com/example/tests/AlsoGroupCreationTests.java
+++ EK-addressbook-tests/src/com/example/tests/AlsoGroupCreationTests.java
@@ -6,21 +6,21 @@ public class AlsoGroupCreationTests extends TestBase {
 	
   @Test
   public void testNonEmptyGroupCreation() throws Exception {
-	app.navigationHelper.openMainPage();
+    app.navigationHelper.openMainPage();
     app.navigationHelper.gotoGroupsPage();
     app.groupHelper.initGroupCreation();
     GroupData group = new GroupData();
     group.name = "group name 1";
     group.header = "header 1";
     group.footer = "footer 1";
-	app.groupHelper.fillGroupForm(app, this, group);
+    app.groupHelper.fillGroupForm(app, this, group);
     app.groupHelper.submitGroupCreation();
     app.groupHelper.returnToGroupsPage();
   }
   
   @Test
   public void testEmptyGroupCreation() throws Exception {
-	app.navigationHelper.openMainPage();
+    app.navigationHelper.openMainPage();
     app.navigationHelper.gotoGroupsPage();
     app.groupHelper.initGroupCreation();
     app.groupHelper.fillGroupForm(app, this, new GroupData("", "", ""));
