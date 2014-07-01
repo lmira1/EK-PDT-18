11156fbd2499f4e02585d13b514651415a3a21d4
lmira1 <elmira.kochetova@gmail.com>
2014-07-01 18:57:05 +0400

    HomeTaskTmp6

---
+++ EK-addressbook-java-tests/src/com/example/fw/FolderHelper.java
@@ -0,0 +1,41 @@
+package com.example.fw;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JDialogOperator;
+import org.netbeans.jemmy.operators.JFrameOperator;
+import org.netbeans.jemmy.operators.JTextFieldOperator;
+import org.netbeans.jemmy.operators.JTreeOperator;
+
+public class FolderHelper {
+
+	private final ApplicationManager manager;
+
+	public FolderHelper(ApplicationManager applicationManager) {
+		this.manager = applicationManager;
+		
+	}
+
+	public Folders getFolders() {
+		List<String> list = new ArrayList<String>();
+		JTreeOperator tree = new JTreeOperator(manager.getApplication());
+		Object[] children = tree.getChildren(tree.getRoot());
+		for (Object child : children) {
+			list.add("" + child);
+		}
+		return new Folders(list);
+	}
+
+	public void createFolder(String folderName) {
+		manager.getMenuHelper().pushCreateFolder();
+		JDialogOperator dialog = new JDialogOperator(manager.getApplication());
+		new JTextFieldOperator(dialog).setText(folderName);
+		new JButtonOperator(dialog, "OK");
+		
+	}
+
+	
+	
+}
---
+++ EK-addressbook-tests/src/com/example/tests/Sample5.java
@@ -0,0 +1,11 @@
+package com.example.tests;
+
+public class Sample5 {
+
+	
+	public static void main(String[] args) {
+		String line = ",,test-999999779,,";
+		System.out.println(line.split(",").length);
+	}
+
+}
